8fb2a1e067b039639beb6ea0212cd4167b767ea9
xyk <a>
2021-04-17 17:24:55 +0800

    第二次提交

--- xihe-gatway/src/main/java/com/example/xihegatway/XiheGatwayApplication.java
+++ xihe-gatway/src/main/java/com/example/xihegatway/XiheGatwayApplication.java
@@ -2,8 +2,10 @@ package com.example.xihegatway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class XiheGatwayApplication {
 
     public static void main(String[] args) {
